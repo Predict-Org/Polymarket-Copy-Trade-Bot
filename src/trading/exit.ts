83d6ed965073f0e21affb7c15373aa2189248556
Vadim <lansibro@gmail.com>
2026-07-25 15:23:48 +0300

    Fix big-decimal-ts package issue

--- src/trading/exit.ts
+++ src/trading/exit.ts
@@ -1,6 +1,6 @@
 import type { ClobClient } from "@polymarket/clob-client";
 import { OrderType, Side } from "@polymarket/clob-client";
-import { Big } from "big-decimal-ts";
+import { Big } from "big.js";
 import type { AppConfig } from "../types";
 import { DATA_API, EXIT_INTERVAL_MS, POSITIONS_MAX_OFFSET, POSITIONS_PAGE_SIZE } from "../constant";
 
